6f9b61d975e06b0a49b57908b8e76c4bf349f7ac
Constantine Vetoshev <gepardcv@gmail.com>
2009-08-06 11:56:43 -0400

    Added the beginnings of low-level routines for database handling.

--- src/leftb-storage/leftb-storage.clj
+++ src/leftb-storage/leftb-storage.clj
@@ -1,21 +1,23 @@
 (ns leftb-storage
   (:require [clojure.contrib.java-utils :as c.c.java-utils])
-  (:import [com.sleepycat.je Environment EnvironmentConfig DatabaseException]))
+  (:import [com.sleepycat.je DatabaseException])
+  (:import [com.sleepycat.je EnvironmentConfig Environment])
+  (:import [com.sleepycat.je DatabaseConfig Database]))
 
 
 (defstruct db-env
-  :directory
-  :configuration
-  :environment
+  :dir
+  :conf
+  :env-handle
   :databases)
 
 
 ;; TODO: Error handling?
-(defn open-db-env [dir & conf-args]
+(defn db-env-open [dir & conf-args]
   (let [defaults {:allow-create false
                   :read-only false
                   :transactional false}
-        dir      (if (instance? java.io.File dir) dir (c.c.java-utils/file dir))
+        dir      (c.c.java-utils/file dir)
         conf-map (merge defaults (apply hash-map conf-args))
         conf     (doto (EnvironmentConfig.)
                    (.setAllowCreate   (conf-map :allow-create))
@@ -23,9 +25,9 @@
                    (.setTransactional (conf-map :transactional)))]
     (when-not (.exists dir) (.mkdir dir)) ; TODO: mkdir -p in Java?
     (struct-map db-env
-      :directory dir
-      :configuration conf
-      :environment (Environment. dir conf)
+      :dir dir
+      :conf conf
+      :env-handle (Environment. dir conf)
       :databases (atom {}))))
 
 
@@ -33,9 +35,12 @@
 
 
 ;; TODO: Error handling?
-(defn close-db-env [db-env]
-  (.cleanLog (db-env :environment))
-  (.close (db-env :environment)))
+(defn db-env-close [db-env]
+  ;; TODO: Close all open database handles in this environment (be
+  ;; sure to use the Clojure db-close function).
+  ;; TODO: Clear out the env-handle afterwards to avoid using it.
+  (.cleanLog (db-env :env-handle))
+  (.close (db-env :env-handle)))
 
 
 ;; TODO: Environment statistics gathering
@@ -45,9 +50,42 @@
 
 
 (defstruct db
-  :environment
+  :env
   :name
-  :configuration)
+  :conf
+  :db-handle)
 
 
-; (defn open-db 
+;; TODO: Error handling?
+(defn db-open [db-env name & conf-args]
+  (when-let [existing-db (contains? @(db-env :databases) name)]
+    ;; flush deferred-write data, close handle, remove handle from db-env
+    (db-close existing-db)
+    (swap! (db-env :databases) dissoc name))
+  (let [defaults {:allow-create false
+                  :deferred-write false
+                  :temporary false}
+        conf-map (merge defaults (apply hash-map conf-args))
+        conf     (doto (DatabaseConfig.)
+                   (.setAllowCreate   (conf-map :allow-create))
+                   (.setDeferredWrite (conf-map :deferred-write)))
+        db       (struct-map db
+                   :env db-env
+                   :name name
+                   :conf conf
+                   :db-handle (.openDatabase
+                               (db-env :env-handle) nil name conf))]
+    ;; Push this database into the (db-env :databases) atom and return
+    ;; the db object.
+    (swap! (db-env :databases) assoc name db)
+    db))
+
+
+(defn db-sync [db]
+  (when (.getDeferredWrite (db :conf))
+    (.sync (db :db-handle))))
+
+
+(defn db-close [db]
+  ;; TODO: Deal with all open cursors on the database.
+  (.close (db :db-handle)))
