e34ddfedc4e3444ee1c9ead9c8c8a5ad3e317a91
Constantine Vetoshev <gepardcv@gmail.com>
2009-08-08 16:47:51 -0400

    Added unmarshaling routines for primitive objects.

--- src/cupboard/marshal.clj
+++ src/cupboard/marshal.clj
@@ -3,28 +3,33 @@
   (:import [com.sleepycat.bind.tuple TupleBinding TupleInput TupleOutput]))
 
 
-(declare unmarshal-db-entry)                  ; TODO: Clean me up.
+;; TODO: This needs a test suite.
 
 
-;; 100 -> [1 :integer 100]
-;; 1.1 -> [1 :float 1.1]
-;; "hello" -> [1 :string(utf-8) "hello"]
-;; [1 2.2] -> [2 :integer 1 :float 2.2]
-;; {:one 1 :two 2.2} -> [2 :kv ["one" :integer 1] :kv ["two" :float 2.2]]
-;;
-;; indexing
+;; TODO: Verify that strings encode in UTF-8 under all circumstances.
+;; TODO: Handle Clojure lists, vectors, maps, and sets.
+;; TODO: Lock out lazy Clojure data structures if possible?
+;; TODO: Handle Java collections?
 
 
-(def type-codes
-     {java.lang.Boolean    1
-      java.lang.Character  2
-      java.lang.Byte       3
-      java.lang.Short      4
-      java.lang.Integer    5
-      java.lang.Long       6
-      java.math.BigInteger 7
-      java.lang.Double     8
-      java.lang.String     9})
+;; TODO: Verify the correctness of using array-map here and the
+;; performance of the lookups (all linear, might be good for such a
+;; small map).
+(def type-codes-by-type
+     (array-map java.lang.Boolean    1
+                java.lang.Character  2
+                java.lang.Byte       3
+                java.lang.Short      4
+                java.lang.Integer    5
+                java.lang.Long       6
+                java.math.BigInteger 7
+                java.lang.Double     8
+                java.lang.String     9
+                clojure.lang.Keyword 10
+                clojure.lang.Symbol  11))
+
+(def type-codes-by-code
+     (zipmap (vals type-codes-by-type) (keys type-codes-by-type)))
 
 
 (defmulti marshal-db-entry class)
@@ -33,7 +38,7 @@
   `(defmethod marshal-db-entry ~java-type [data#]
      (let [db-entry#     (DatabaseEntry.)
            tuple-output# (TupleOutput.)]
-       (.writeUnsignedByte tuple-output# (type-codes ~java-type))
+       (.writeUnsignedByte tuple-output# (type-codes-by-type ~java-type))
        (~write-method tuple-output# data#)
        (TupleBinding/outputToEntry tuple-output# db-entry#)
        db-entry#)))
@@ -48,3 +53,26 @@
 (def-primitive-marshal-method java.math.BigInteger .writeBigInteger)
 (def-primitive-marshal-method java.lang.Double .writeSortedDouble)
 (def-primitive-marshal-method java.lang.String .writeString)
+
+
+(defmulti unmarshal-db-entry-helper (fn [native-type _] native-type))
+
+(defmacro def-primitive-unmarshal-helper-method [java-type read-method]
+  `(defmethod unmarshal-db-entry-helper ~java-type [native-type# tuple-input#]
+     (~read-method tuple-input#)))
+
+(def-primitive-unmarshal-helper-method java.lang.Boolean .readBoolean)
+(def-primitive-unmarshal-helper-method java.lang.Character .readChar)
+(def-primitive-unmarshal-helper-method java.lang.Byte .readByte)
+(def-primitive-unmarshal-helper-method java.lang.Short .readShort)
+(def-primitive-unmarshal-helper-method java.lang.Integer .readInteger)
+(def-primitive-unmarshal-helper-method java.lang.Long .readLong)
+(def-primitive-unmarshal-helper-method java.math.BigInteger .readBigInteger)
+(def-primitive-unmarshal-helper-method java.lang.Double .readSortedDouble)
+(def-primitive-unmarshal-helper-method java.lang.String .readString)
+
+(defn unmarshal-db-entry [db-entry]
+  (let [tuple-input (TupleBinding/entryToInput db-entry)
+        type-byte   (.readUnsignedByte tuple-input)
+        native-type (type-codes-by-code type-byte)]
+    (unmarshal-db-entry-helper native-type tuple-input)))
