e22991fdee080604d641dd3156e8902ed9f80e99
Constantine Vetoshev <gepardcv@gmail.com>
2009-08-21 15:26:23 -0400

    Refactored close-shelves into a more flexible combination of functions. Write a first cut of get-shelf.

--- src/cupboard.clj
+++ src/cupboard.clj
@@ -42,12 +42,74 @@
 ;;; cupboard maintenance
 ;;; ----------------------------------------------------------------------
 
+(defn- get-shelf
+  "Returns the shelf identified by shelf-name from the cupboard
+  identified by cb. If the shelf is not open, open and return it. If
+  the shelf does not exist, then create, open, and return it."
+  [cb shelf-name & opts-args]
+  (let [defaults {:deferred-write    false
+                  :sorted-duplicates false
+                  :read-only         false
+                  :transactional     true
+                  :force-reopen      false}
+        opts     (merge defaults (args-map opts-args))]
+    (when (opts :force-reopen)
+      (close-shelf cb shelf-name))
+    (if (contains? @(cb :shelves) shelf-name)
+        ;; shelf is ready and open, just return it
+        ((cb :shelves) shelf-name)
+        ;; no shelf found in cupboard, need to either open or create it
+        (let [shelf-desc (db-get (cb :shelves-db) shelf-name)]
+          (if (= shelf-desc [])
+              ;; shelf does not exist --- create a new one
+              ;; TODO: Refactor into a separate function, and use in init-cupboard
+              (let [new-shelf-opts {:deferred-write    (opts :deferred-write)
+                                    :sorted-duplicates (opts :sorted-duplicates)
+                                    :read-only         (opts :read-only)
+                                    :transactional     (opts :transactional)}
+                    new-shelf-db   (db-open (cb :cb-env) shelf-name
+                                            (assoc new-shelf-opts :allow-create true))
+                    new-shelf      (struct shelf new-shelf-db shelf-name (atom {}))]
+                (db-put (cb :shelves-db) shelf-name (assoc new-shelf-opts :index-names []))
+                (swap! (cb :shelves) assoc shelf-name new-shelf)
+                new-shelf)
+              ;; shelf exists --- open it
+              ;; TODO: Refactor into a separate function, and use in init-cupboard
+              (let [[_ shelf-opts] shelf-desc
+                    ;; Careful on merge here! Only use explicitly specified arguments!
+                    open-opts      (merge shelf-opts opts-args)
+                    shelf-db       (db-open (cb :cb-env) shelf-name open-opts)
+                    ;; TODO: Open indices!
+                    shelf          (struct shelf shelf-db shelf-name (atom {}))]
+                (swap! (cb :shelves) assoc shelf-name shelf)
+                shelf))))))
+
+
 ;; TODO: Check that index closing actually works.
-(defn- close-shelves [shelves]
-  (doseq [shelf (vals shelves)]
-    (doseq [index-db (vals @(shelf :index-dbs))]
-      (db-close (index-db :db)))
-    (db-close (shelf :db))))
+(defn- close-shelf
+  "The two-parameter form of close-shelf closes shelves and removes
+  them from the cupboard (cb argument). The one-parameter form just
+  closes the given shelf, without doing any extra cleanup."
+  ([shelf]
+     (doseq [index-db (vals @(shelf :index-dbs))]
+       (db-close (index-db :db)))
+     (db-close (shelf :db)))
+  ([cb shelf-name]
+     (close-shelf (@(cb :shelves) shelf-name))
+     (swap! (cb :shelves) dissoc shelf-name)))
+
+
+(defn- close-shelves
+  "If arg is a cupboard instance, this closes all cupboard shelves and
+  removes them from the :shelves table in the cupboard. If arg is just
+  a table of shelves, it simply closes them."
+  [arg]
+  (if (is-cupboard? arg)
+      (doseq [shelf-name (vals @(arg :shelves))]
+        (close-shelf arg shelf-name))
+      ;; this is for use from the init-cupboard error recovery block
+      (doseq [shelf (vals arg)]
+        (close-shelf shelf))))
 
 
 (defn- init-cupboard [cb-env cb-env-new]
