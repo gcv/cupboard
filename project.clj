0bf13b6e9671f8be532ac3f72bdc745977b90ea5
Sergey Pariev <spariev@undev.ru>
2012-02-23 13:40:26 +0400

    switching to 1.3, removing contrib dependency

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject cupboard "1.0.0-SNAPSHOT"
   :description "An embedded database library for Clojure."
   :repositories {"oracle" "http://download.oracle.com/maven"}
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+
                  [com.sleepycat/je "4.0.92"]
                  [joda-time "1.6.2"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
