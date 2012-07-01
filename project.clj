02eef82f3fa38a5961ca9bf6cc560f6d0c685864
Constantine Vetoshev <gepardcv@gmail.com>
2012-07-01 09:19:54 -0700

    Modernized dependencies.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject cupboard "1.0beta1"
   :description "An embedded database library for Clojure."
   :repositories {"oracle" "http://download.oracle.com/maven"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.sleepycat/je "4.0.92"]
-                 [joda-time "1.6.2"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.sleepycat/je "5.0.55"]
+                 [joda-time "2.1"]]
   :dev-dependencies [[swank-clojure "1.4.0"]])
