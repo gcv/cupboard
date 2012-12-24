e64bdc3e19028957c143126a6be8a39419698b2f
Constantine Vetoshev <gepardcv@gmail.com>
2012-12-23 19:41:28 -0800

    Looks like a new version of BDB JE is out.

--- project.clj
+++ project.clj
@@ -2,6 +2,5 @@
   :description "An embedded database library for Clojure."
   :repositories {"oracle" "http://download.oracle.com/maven"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.sleepycat/je "5.0.55"]
-                 [joda-time "2.1"]]
-  :dev-dependencies [[swank-clojure "1.4.0"]])
+                 [com.sleepycat/je "5.0.58"]
+                 [joda-time "2.1"]])
